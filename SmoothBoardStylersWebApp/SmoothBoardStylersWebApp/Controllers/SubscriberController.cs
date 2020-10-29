269b6a4e6d4b5e91bb1f29d7b16303953df0226a
Thomas <thomasreuvers@upcmail.nl>
2020-10-29 16:32:17 +0100

    Homepage styling and _layout shared styles

---
+++ SmoothBoardStylersWebApp/SmoothBoardStylersWebApp/Controllers/SubscriberController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmoothBoardStylersWebApp.Controllers
+{
+    public class SubscriberController : Controller
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> AddSubscriber()
+        {
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ SmoothBoardStylersWebApp/SmoothBoardStylersWebApp/Models/SubscriberViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmoothBoardStylersWebApp.Models
+{
+    public class SubscriberViewModel
+    {
+        [Required]
+        public string Firstname { get; set; }
+
+        [Required]
+        public string Lastname { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        public Sex Sex { get; set; }
+    }
+}
