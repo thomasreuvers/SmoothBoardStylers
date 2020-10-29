99b7fc5b4a1072908b275dde42228008e7817b65
Thomas <thomasreuvers@upcmail.nl>
2020-10-29 16:09:02 +0100

    Update LoginViewModel.cs

--- SmoothBoardStylersWebApp/SmoothBoardStylersWebApp/Models/LoginViewModel.cs
+++ SmoothBoardStylersWebApp/SmoothBoardStylersWebApp/Models/LoginViewModel.cs
@@ -9,8 +9,10 @@ namespace SmoothBoardStylersWebApp.Models
 {
     public class LoginViewModel
     {
+        [Required]
         public string Username { get; set; }
 
+        [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
